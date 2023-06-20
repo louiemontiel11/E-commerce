168b9e97f9a3238f909eaab84dacaac7f10f7d6b
new name <louie.montiel@accenture.com>
2023-06-20 14:22:54 +0800

    push

--- E-commerce/src/app/app.module.ts
+++ E-commerce/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
-=======
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
