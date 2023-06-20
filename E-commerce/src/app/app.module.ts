cc0f2b43632ecb47d720a8538826645a5fdfba18
new name <louie.montiel@accenture.com>
2023-06-20 14:27:36 +0800

    create banner component

--- E-commerce/src/app/app.module.ts
+++ E-commerce/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './home/banner/banner.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
